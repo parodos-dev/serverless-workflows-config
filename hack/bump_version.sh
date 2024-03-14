6d43774268767350244647f8c1805e4c36fd9f43
Moti Asayag <masayag@redhat.com>
2024-03-14 03:36:41 +0200

    Refactor bump_version script
    - reuse bump version logic
    - add INFO messages
    - update main chart version
    
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- hack/bump_version.sh
+++ hack/bump_version.sh
@@ -2,17 +2,13 @@
 
 # Function to bump the version
 bump_version() {
-    local chart_file="$1"
-    local line_name="$2"
-    local increment_type="$3"
-    
-    # Get current version
-    local version_line=$(grep "^${line_name}" "${chart_file}")
-    local current_version=$(echo "${version_line}" | awk '{print $2}')
+    local current_version="$1"
+    local increment_type="$2"
+
     local major=$(echo "${current_version}" | cut -d '.' -f1)
     local minor=$(echo "${current_version}" | cut -d '.' -f2)
     local patch=$(echo "${current_version}" | cut -d '.' -f3)
-    
+
     # Increment version based on type
     case "${increment_type}" in
         minor)
@@ -33,13 +29,12 @@ bump_version() {
             ;;
     esac
 
-    new_version="${major}.${minor}.${patch}"
-    sed -i "s/^${line_name} .*/${line_name} ${new_version}/" "${chart_file}"
+    echo "${major}.${minor}.${patch}"
 }
 
 # Function to display usage
 usage() {
-    echo "Usage: $0 <dependency1>:<increment_type1> <dependency2>:<increment_type2> ..."
+    echo "Usage: $0 <dependency1>:<increment_type1> <dependency2>:<increment_type2> ... <main_chart_increment>"
     echo "Dependency names must match the folder names of the dependency charts."
     echo "Increment type options: patch, minor, major"
 }
@@ -54,45 +49,68 @@ fi
 # File path to the main Chart.yaml
 main_chart_file="charts/workflows/Chart.yaml"
 
-# Parse arguments
-for arg in "$@"; do
+# Parse arguments for dependencies
+dependencies_args="${@:1:$#-1}"
+main_chart_increment="${@: -1}"
+
+# Parse dependencies arguments
+for arg in $dependencies_args; do
     IFS=':' read -r -a parts <<< "$arg"
     if [ ${#parts[@]} -ne 2 ]; then
-        echo "Error: Invalid argument format: $arg"
+        echo "ERROR: Invalid argument format: $arg"
         usage
         exit 1
     fi
     dependency_name="${parts[0]}"
     increment_type="${parts[1]}"
-    
     # Check if increment type is valid
     case "${increment_type}" in
         patch|minor|major)
             ;;
         *)
-            echo "Error: Invalid increment type: ${increment_type}"
+            echo "ERROR: Invalid increment type for dependency ${dependency_name}: ${increment_type}"
             usage
             exit 1
             ;;
     esac
-    
-    # Get the current version of the dependency
-    dependency_version=$(grep "^ *- name: ${dependency_name}" "${main_chart_file}" | awk '/version:/{print $2}')
 
-    # Bump the dependency version
-    bump_version "charts/workflows/charts/${dependency_name}/Chart.yaml" "version:" "${increment_type}"
+    # Get the current version of the dependency
+    dependency_version=$(awk -v dep="$dependency_name" '
+            /^ *- name: / { in_dep = ($3 == dep) }
+            in_dep && /^ *version:/ { gsub(/"/, "", $2); print $2; exit }
+            ' ${main_chart_file})
+
+    # Get the value for the new dependency version
+    new_dependency_version=$(bump_version "${dependency_version}" "${increment_type}")
+    echo "INFO: Dependency ${dependency_name} with version ${dependency_version} is updated to ${new_dependency_version}"
 
     # Update the dependency version in the main Chart.yaml
-    sed -i "s|^ *- name: ${dependency_name}.*|  - name: ${dependency_name}\n    version: \"${dependency_version}\"|" "${main_chart_file}"
+    sed -i "/^ *- name: ${dependency_name}/,/^ *- /s/version: .*/version: \"${new_dependency_version}\"/" "${main_chart_file}"
+
+    # Update the dependency version in the dependency Chart.yaml
+    chart_file="charts/workflows/charts/${dependency_name}/Chart.yaml"
+    attribute_name="version:"
+    sed -i "s/^${attribute_name} .*/${attribute_name} ${new_dependency_version}/" "${chart_file}"
 done
 
-# Bump version in main Chart.yaml after updating dependencies
-for arg in "$@"; do
-    IFS=':' read -r -a parts <<< "$arg"
-    dependency_name="${parts[0]}"
-    increment_type="${parts[1]}"
-    bump_version "${main_chart_file}" "version:" "${increment_type}"
-    break
-done
+# Bump version of the main chart
+case "${main_chart_increment}" in
+    patch|minor|major)
+        ;;
+    *)
+        echo "ERROR: Invalid increment type for main chart: ${main_chart_increment}"
+        usage
+        exit 1
+        ;;
+esac
+
+main_chart_version=$(grep "^version:" "${main_chart_file}" | awk '{print $2}')
+new_main_chart_version=$(bump_version "${main_chart_version}" "${main_chart_increment}")
+sed -i "s/^version: .*/version: ${new_main_chart_version}/" "${main_chart_file}"
+echo "INFO: Main chart workflows with version ${main_chart_version} is updated to ${new_main_chart_version}"
+echo
+
+# Update the dependencies
+helm dep update charts/workflows/
 
 echo "Chart and dependency versions updated successfully."
